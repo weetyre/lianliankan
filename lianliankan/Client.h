5564fba0ea932a2c9127bcf11fde9f1d66f8ad9e
yyl <2454453499@qq.com>
2018-06-10 16:39:58 +0800

    注册和登录完成

---
+++ lianliankan/Client.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<winsock2.h>  
+#include<stdio.h>  
+#include<windows.h>  
+#pragma comment(lib,"WS2_32.lib")  
+using namespace std;
+
+int hasifOnserver(string id);
+
+int addIDOnserver(string id);
